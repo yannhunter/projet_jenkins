6a91e682db39575e76d6a2914edc6f191ce40460
yannhunter <57797157+yannhunter@users.noreply.github.com>
2022-11-20 00:38:18 +0100

    modification du code

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
-package com.xavki;
-public class HelloWorld {
+public class Main {
   public static void main(String[] args) {
-    HelloWorld myWorld = new HelloWorld();
-    String said = myWorld.sayHelloTo("World");
-    System.out.println(said);
-    }
-  public String sayHelloTo(String to) {
-    return "Hello "+to+"!";
+    System.out.println("Hello,yann " );
     }
 }
